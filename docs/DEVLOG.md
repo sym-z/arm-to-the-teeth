e585eb5a89c5018fcbd01a115a3e826b92b0a6cd
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-08-13 19:57:48 -0700

    main menu high score

--- docs/DEVLOG.md
+++ docs/DEVLOG.md
@@ -362,3 +362,4 @@
 - Scene is coded up for the Tutorial scene, and linked it to the main menu. Still need to add the text for it and test it.
 - Quit button in UI now works
 - High score is tracked and shown in Death Screen
+- High score is now displayed in the main menu
--- src/scenes/main_menu/main_menu.gd
+++ src/scenes/main_menu/main_menu.gd
@@ -4,10 +4,13 @@ extends CanvasLayer
 @export var tutorial_button : Button
 @export var quit_button : Button
 
+@export var score_label : Label
+
 func _ready():
 	play_button.connect("pressed", start_game)
 	quit_button.connect("pressed", quit_game)
 	tutorial_button.connect("pressed", tutorial)
+	score_label.text = "HIGHEST FLOOR REACHED: " + str(Globals.highest_floor)
 
 func start_game():
 	Globals.curr_floor = 0
