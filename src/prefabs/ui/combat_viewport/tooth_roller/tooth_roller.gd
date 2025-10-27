4e0e46e336ff44060207bbee5a73ee94f768abcb
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-10-27 12:53:58 -0700

    beginning encoding target pool

--- src/prefabs/ui/combat_viewport/tooth_roller/tooth_roller.gd
+++ src/prefabs/ui/combat_viewport/tooth_roller/tooth_roller.gd
@@ -1,18 +1,28 @@
 extends Node2D
 
 @export var tooth_targets : Array[AnimatedSprite2D]
+@export var shift_timer : Timer
+# How many teeth roll in.
+@export var target_pool_size : int = 25
+var target_pool : Array[int] = []
+# How many frames the targets have
+var tooth_types : int = 3
 
-## Initial Idea
-#func _ready():
-	#for tooth in tooth_targets:
-		#tooth.set_frame(2)
+func _ready():
+	
+	shift_timer.connect("timeout", shift_teeth)
+	
+func create_target_pool(size : int):
+	for num in size:
+		target_pool.append(randi_range(0,tooth_types))
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
-		#for tooth in tooth_targets:
-			#tooth.set_frame(2)
 		tooth_targets[0].set_new_frame(2)
 	elif event.is_action_pressed("ui_down"):
-		#for tooth in tooth_targets:
-			#tooth.set_frame(1)
 		tooth_targets[0].set_new_frame(1)
+
+
+func shift_teeth():
+	pass
